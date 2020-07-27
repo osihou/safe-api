43cb5e81a496b217ad9c661e5b450839f8814bee
0keonx <kenan.majewski@gmail.com>
2020-07-27 21:05:46 +0200

    soo close

--- src/main/java/com/projectpl/safeapi/entity/Location.java
+++ src/main/java/com/projectpl/safeapi/entity/Location.java
@@ -8,7 +8,7 @@ import java.io.Serializable;
 public class Location implements Serializable {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
 
     private String name;
