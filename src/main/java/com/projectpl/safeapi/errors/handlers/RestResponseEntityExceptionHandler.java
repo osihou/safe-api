b27a12044d540034c30e66c6b0ba350b614a0ac2
0keonx <kenan.majewski@gmail.com>
2020-08-09 22:53:46 +0200

    REST Response Handler

---
+++ src/main/java/com/projectpl/safeapi/errors/handlers/RestResponseEntityExceptionHandler.java
@@ -0,0 +1,123 @@
+package com.projectpl.safeapi.errors.handlers;
+
+import com.projectpl.safeapi.errors.exceptions.UserAlreadyExistException;
+import com.projectpl.safeapi.errors.exceptions.UserNotFoundException;
+import com.projectpl.safeapi.utils.GenericResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.mail.MailAuthenticationException;
+import org.springframework.validation.BindException;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @Autowired
+    private MessageSource messages;
+
+    @Override
+    protected ResponseEntity<Object> handleBindException(final BindException ex, final HttpHeaders headers, final HttpStatus status, final WebRequest request) {
+        logger.error("400 Status Code", ex);
+
+        final BindingResult result = ex.getBindingResult();
+
+        final GenericResponse bodyOfResponse = new GenericResponse(
+                result.getAllErrors(),
+                "Invalid" + result.getObjectName());
+
+        return handleExceptionInternal(
+                ex,
+                bodyOfResponse,
+                new HttpHeaders(),
+                HttpStatus.BAD_REQUEST,
+                request
+        );
+    }
+
+    @ExceptionHandler({ UserNotFoundException.class })
+    public ResponseEntity<Object> handleUserNotFound(RuntimeException ex, WebRequest request) {
+        logger.error("404 Status Code", ex);
+
+        GenericResponse bodyOfResponse = new GenericResponse(
+                messages.getMessage(
+                        "message.userNotFound",
+                        null, request.getLocale()),
+                "UserNotFound"
+        );
+
+        return handleExceptionInternal(
+                ex,
+                bodyOfResponse,
+                new HttpHeaders(),
+                HttpStatus.NOT_FOUND,
+                request
+        );
+    }
+
+    @ExceptionHandler({ UserAlreadyExistException.class })
+    public ResponseEntity<Object> handleUserAlreadyExist(final RuntimeException ex, final WebRequest request) {
+        logger.error("409 Status Code", ex);
+        final GenericResponse bodyOfResponse = new GenericResponse(
+                messages.getMessage(
+                        "message.regError",
+                        null,
+                        request.getLocale()),
+                "UserAlreadyExist"
+        );
+
+        return handleExceptionInternal(
+                ex,
+                bodyOfResponse,
+                new HttpHeaders(),
+                HttpStatus.CONFLICT,
+                request
+        );
+    }
+
+    @ExceptionHandler({ MailAuthenticationException.class })
+    public ResponseEntity<Object> handleMail(RuntimeException ex, WebRequest request) {
+        logger.error("500 Status Code", ex);
+
+        GenericResponse bodyOfResponse = new GenericResponse(
+                messages.getMessage(
+                        "message.email.config.error",
+                        null,
+                        request.getLocale()),
+                "MailError");
+
+        return handleExceptionInternal(
+                ex,
+                bodyOfResponse,
+                new HttpHeaders(),
+                HttpStatus.NOT_FOUND,
+                request
+        );
+    }
+
+    @ExceptionHandler({ Exception.class })
+    public ResponseEntity<Object> handleInternal(RuntimeException ex, WebRequest request) {
+        logger.error("500 Status Code", ex);
+        GenericResponse bodyOfResponse = new GenericResponse(
+                messages.getMessage(
+                        "message.error",
+                        null,
+                        request.getLocale()),
+                "InternalError"
+        );
+
+        return handleExceptionInternal(
+                ex,
+                bodyOfResponse,
+                new HttpHeaders(),
+                HttpStatus.NOT_FOUND,
+                request
+        );
+    }
+}
