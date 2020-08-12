b90d56053aaccc23127c6d6921c17796373e267c
DanielGaszczyk <daniel.gaszzczyk@gmail.com>
2020-08-12 18:24:56 +0200

    Added:  -photos upload  -adding products

---
+++ src/main/java/com/projectpl/safeapi/service/product/ProductController.java
@@ -0,0 +1,32 @@
+package com.projectpl.safeapi.service.product;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import com.projectpl.safeapi.persistance.entity.Product;
+import com.projectpl.safeapi.service.product.ProductService;
+
+@Controller
+@RequestMapping("product")
+public class ProductController {
+
+    @Autowired
+    private ProductService productService;
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String index(ModelMap modelMap) {
+        modelMap.put("product", new Product());
+        return "product/index";
+    }
+
+    @RequestMapping(value = "save", method = RequestMethod.POST)
+    public String save(@ModelAttribute("product") Product product, ModelMap modelMap) {
+        Product newProduct = productService.create(product);
+        modelMap.put("newProduct", newProduct);
+        return "product/success";
+    }
+
+}
